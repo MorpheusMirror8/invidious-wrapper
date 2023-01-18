0e862644f2598b2c018e876e1f3a23b53badf137
MorpheusMirror8 <122835021+MorpheusMirror8@users.noreply.github.com>
2023-01-18 00:41:12 -0600

    Add files via upload

--- invidious-wrapper2/docker_entrypoint.sh
+++ invidious-wrapper2/docker_entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 echo "Starting Invidious..."
-exec tini invidious
+
+#!/bin/bash
+set -eou pipefail
+su - postgres -c "pg_ctl -D /var/lib/postgresql/data start"
+#RUN find /invidious/config/sql -name '*.sql' -exec psql -U postgres -d invidious -f {} \;
+
+
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/channels.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/videos.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/channel_videos.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/users.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/session_ids.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/nonces.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/annotations.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/playlists.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/playlist_videos.sql
+
+exec tini /invidious/invidious
+
